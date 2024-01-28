1261850083c50dcea5f846c233e1a9da643ed1e1
granth2023 <grantharris38@gmail.com>
2024-01-28 14:29:28 -0500

    import generics

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import Project 
 from .models import Project 
 from .serializers import ProjectSerializer
+from rest_framework import generics
 # Create your views here.
 class ProjectList(generics.ListCreateAPIView):
     queryset = Project.objects.all()
