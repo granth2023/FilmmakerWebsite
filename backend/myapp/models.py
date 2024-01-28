c4f0b5d0437ac56d344621991dea46a25b6895f1
granth2023 <grantharris38@gmail.com>
2024-01-27 22:46:27 -0500

    fields added

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    tit
+    tite
+    script
+    createor_note
+    link 
+    password_access 
+    comprabale_titles
