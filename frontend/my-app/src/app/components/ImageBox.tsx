7dab8e73705991b580b7bb65674812e659b734d2
granth2023 <grantharris38@gmail.com>
2024-01-28 15:11:19 -0500

    added div

--- frontend/my-app/src/app/components/ImageBox.tsx
+++ frontend/my-app/src/app/components/ImageBox.tsx
@@ -6,6 +6,10 @@ interface ImageBoxProps {
 
 const ImageBox: React.FC<ImageBoxProps> = ({ altText }) => {
     console.log('rendering imagbox', altText)
-    
+    return(
+        <div className="m-4 shadow-log broder-4 border-black" style={{ width: '300px', height: '300px' }}>
+            <di
+        </div>
+    )
     
     </ImageBoxProps>
