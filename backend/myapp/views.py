803922ba5f55285c45ad853bea60fb899b2c07b7
granth2023 <grantharris38@gmail.com>
2024-01-28 14:26:50 -0500

    query set added to project list

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -3,3 +3,5 @@ from rest_framework import Project
 from .models import Project 
 from .serializers import ProjectSerializer
 # Create your views here.
+class ProjectList(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
