6cea0b4ccafa3122a425e8e082bf173863226e42
granth2023 <grantharris38@gmail.com>
2024-01-27 22:51:04 -0500

    password access boolean field

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -6,5 +6,5 @@ class Project(models.Model):
     script = models.FileField('/scripts', null=True, blank=True)
     createor_note = models.TextField()
     link = models.URLField(blank=True, null=True)
-    password_access = 
+    password_access = models.BooleanField(default=False)
     comprabale_titles = models.TextField()
