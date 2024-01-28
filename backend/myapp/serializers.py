ee7d3981c21bb34675b651d767fdc3a28de41bd5
granth2023 <grantharris38@gmail.com>
2024-01-28 14:24:23 -0500

    project serializer added

--- backend/myapp/serializers.py
+++ backend/myapp/serializers.py
@@ -2,3 +2,6 @@ from rest_framework import serializers
 from .models import myapp 
 
 class Projectserializer(serializers.ModelSerializer):
+    class Meta: 
+        model = myapp 
+        fields ='__all__'
