94f2f9f551253d15a026fcf9bbc1d3c0520405b5
granth2023 <grantharris38@gmail.com>
2024-01-27 22:49:28 -0500

    script file field created

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     tite = models.CharField(max_length=200)
-    script = 
+    script = models.FileField('/scripts', null=True, blank=True)
     createor_note = models.TextField()
     link 
     password_access 
