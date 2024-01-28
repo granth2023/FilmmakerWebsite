ce18a167e2b79177c91b2e6d0ed4cac49826a451
granth2023 <grantharris38@gmail.com>
2024-01-27 22:48:43 -0500

    creator note text field

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     tite = models.CharField(max_length=200)
-    script 
-    createor_note
+    script = 
+    createor_note = models.TextField()
     link 
     password_access 
     comprabale_titles
