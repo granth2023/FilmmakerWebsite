4698fa61d266c9630fa61b73654e48c3592f3ef2
granth2023 <grantharris38@gmail.com>
2024-01-27 22:50:11 -0500

    link url field created

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -5,6 +5,6 @@ class Project(models.Model):
     tite = models.CharField(max_length=200)
     script = models.FileField('/scripts', null=True, blank=True)
     createor_note = models.TextField()
-    link 
+    link = models.URLField(blank=True, null=True)
     password_access 
     comprabale_titles
