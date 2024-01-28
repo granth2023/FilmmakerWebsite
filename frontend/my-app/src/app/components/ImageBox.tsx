e67eaa25b1e7885094e4c8a50886457bdc694a14
granth2023 <grantharris38@gmail.com>
2024-01-28 15:10:25 -0500

    console long and start image box fucntion

--- frontend/my-app/src/app/components/ImageBox.tsx
+++ frontend/my-app/src/app/components/ImageBox.tsx
@@ -3,3 +3,9 @@ import React from 'react';
 interface ImageBoxProps {
     altTexT: string;
 }
+
+const ImageBox: React.FC<ImageBoxProps> = ({ altText }) => {
+    console.log('rendering imagbox', altText)
+    
+    
+    </ImageBoxProps>
