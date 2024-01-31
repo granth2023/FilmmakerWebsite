c66d8877e592453e2c2ba41f81ccd6b0a28b0620
granth2023 <grantharris38@gmail.com>
2024-01-30 21:08:44 -0500

    imports updated

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
 
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
 # Create your models here.
 class Project(models.Model):
     tite = models.CharField(max_length=200)
