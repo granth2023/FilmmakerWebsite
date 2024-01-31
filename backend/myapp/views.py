920be414171f697aefd7515648bdec7b7128b5ec
granth2023 <grantharris38@gmail.com>
2024-01-30 21:56:47 -0500

    import setting and json response

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -3,6 +3,9 @@ from rest_framework import Project
 from .models import Project 
 from .serializers import ProjectSerializer
 from rest_framework import generics
+import requests  
+import django.conf import settings
+from django.http import JsonResponse
 # Create your views here.
 class ProjectList(generics.ListCreateAPIView):
     queryset = Project.objects.all()
