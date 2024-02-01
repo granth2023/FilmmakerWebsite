5be8b4d1911a3454bd67bcce08c37fd6e6b2d545
granth2023 <grantharris38@gmail.com>
2024-01-31 21:35:10 -0500

    movie model test case completed

--- backend/myapp/tests.py
+++ backend/myapp/tests.py
@@ -3,3 +3,13 @@ from .models import Movie, MovieCollection, Event, User, Comment, DiscussionBoar
 # Create your tests here.
 
 
+class MovieModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        
+        Movie.objects.create(title='Titanic', year='1997')
+        
+    def test_title_content(self):
+        movie = Movie.objects.get(id=1)
+        expected_object_name = f'{movie.title}'
+        self.assertEquals(expected_object_name, 'Titanic')
