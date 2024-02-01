df027e11d77b764433a561b3d407d02936a009c7
granth2023 <grantharris38@gmail.com>
2024-01-31 21:20:44 -0500

    reigster collections route

--- backend/mybackenddjango/urls.py
+++ backend/mybackenddjango/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from myapp.views import home, search_movie, MovieViewSet, MovieCollectionViewSet, EventViewSet, DiscussionBaordViewSet, CommentViewSet, LikeCreateDestroyView, RSVPViewSet 
 from rest_framework.routers import DefaultRouter 
 router.register(r'movies', MovieViewSet)
-
+router.register(r'collections', MovieCollectionViewSet)
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
