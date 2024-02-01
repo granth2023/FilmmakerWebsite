47ace829930cacc77025b3111e073d7765de107f
granth2023 <grantharris38@gmail.com>
2024-01-31 21:22:24 -0500

    router register

--- backend/mybackenddjango/urls.py
+++ backend/mybackenddjango/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myapp.views import home, search_movie, MovieViewSet, MovieCollectionViewSet, EventViewSet, DiscussionBaordViewSet, CommentViewSet, LikeCreateDestroyView, RSVPViewSet 
 from rest_framework.routers import DefaultRouter 
 router.register(r'movies', MovieViewSet)
 router.register(r'collections', MovieCollectionViewSet)
+router.register(r'events', EventViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'likes', LikeCreateDestroyView, basename='like')
+router.register(r'rsvps', RSVPViewSet)
+router.register(r'discussionboards', DiscussionBoardViewSet)
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
