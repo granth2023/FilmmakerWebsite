5ba4d610c59ccfcfd4cf1a20cad724f2849bdc64
granth2023 <grantharris38@gmail.com>
2024-01-31 21:19:48 -0500

    import view sets into url

--- backend/mybackenddjango/urls.py
+++ backend/mybackenddjango/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import home, search_movie
+from myapp.views import home, search_movie, MovieViewSet, MovieCollectionViewSet, EventViewSet, DiscussionBaordViewSet, CommentViewSet, LikeCreateDestroyView, RSVPViewSet 
+from rest_framework.routers import DefaultRouter 
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
