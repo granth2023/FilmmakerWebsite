7c00e735a48a61ab7dc8ebe34cfec4583a117dd3
granth2023 <grantharris38@gmail.com>
2024-02-05 13:37:09 -0500

    user model test case created

--- backend/myapp/tests.py
+++ backend/myapp/tests.py
@@ -5,6 +5,10 @@ from .models import Movie, Review, Event
 User = get_user_model()
 
 class UserModelTest(TestCase):
+    def test_user_creation(self):
+        user = User.objects.create_user(email='user@example.com', password='testpass')
+        self.assertEqual(user.email, 'user@example.com')"
+    "
     
     
 class MovieModelTest(TestCase):
