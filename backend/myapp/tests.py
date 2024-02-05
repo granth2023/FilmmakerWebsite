05876848c7970023b6382cbeaaab894c9e066eec
granth2023 <grantharris38@gmail.com>
2024-02-05 13:39:12 -0500

    movie test model

--- backend/myapp/tests.py
+++ backend/myapp/tests.py
@@ -7,11 +7,16 @@ User = get_user_model()
 class UserModelTest(TestCase):
     def test_user_creation(self):
         user = User.objects.create_user(email='user@example.com', password='testpass')
-        self.assertEqual(user.email, 'user@example.com')"
-    "
+        self.assertEqual(user.email, 'user@example.com')
     
     
 class MovieModelTest(TestCase):
+    def setUp(self):
+        Movie.objects.create(title="Titanic", year="1997", director="James Cameron")
+    
+    def test_movie_creation(self):
+        movie = Movie.objects.get(title="Titanic")
+        self.assertEqual(movie.year, "1997")
     
 
 class EventModelTest(TestCase):
