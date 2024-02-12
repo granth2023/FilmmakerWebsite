4abe31e72b4955f6ba659e82f70c2ba88b1968ad
granth2023 <grantharris38@gmail.com>
2024-02-12 12:42:42 -0500

    calss for user accounttests

---
+++ backend/myapp/test_api.py
@@ -0,0 +1,17 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User 
+from .models import Movie, Review 
+
+class UserAccountTests(APITestCase):
+    
+    def test_user_registration(self):
+    
+    
+    def test_user_login(self):
+        
+
+class MovieTests(APITestCase):
+    
+    
