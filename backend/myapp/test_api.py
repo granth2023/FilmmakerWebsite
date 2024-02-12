9e90ba0de51b90a65f83874750957f8cfee31ad4
granth2023 <grantharris38@gmail.com>
2024-02-12 12:46:04 -0500

    register user test

--- backend/myapp/test_api.py
+++ backend/myapp/test_api.py
@@ -7,7 +7,15 @@ from .models import Movie, Review
 class UserAccountTests(APITestCase):
     
     def test_user_registration(self):
-    
+        """ 
+        Ensure we can create a new user account 
+        """
+        url = reverse('register')
+        data = {'username': 'testuser', 'email': 'testuser@example.com', 'password': 'testpassword'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue('access' in response.data)
+        self.assertTrue('refrresh' in response.data)    
     
     def test_user_login(self):
         
