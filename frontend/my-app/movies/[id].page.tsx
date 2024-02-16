beba973225ede63431aa448e8cfbe1cb6fa05e3f
granth2023 <grantharris38@gmail.com>
2024-02-15 20:06:25 -0500

    fetch request added to movie variable

--- frontend/my-app/movies/[id].page.tsx
+++ frontend/my-app/movies/[id].page.tsx
@@ -2,6 +2,7 @@ import { fetchMovieById } from '../utils/movieService';
 
 export const getServerSideProps = async ({ params }) => {
     try {
+        const movie = await fetchMovieById(params.id);
 
     } catch (error) {
         return { props: { error: error.message}};
