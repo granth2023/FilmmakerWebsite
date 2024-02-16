f3382f679c72e184b1ee204487a3e2b4121e95f5
granth2023 <grantharris38@gmail.com>
2024-02-15 19:57:43 -0500

    use effect for movies page

--- frontend/my-app/movies/index.page.tsx
+++ frontend/my-app/movies/index.page.tsx
@@ -1,2 +1,10 @@
 import { useEffect, useState } from 'react';
 import { fetchMovies } from '../utils/movieService';
+
+const MoviesPage = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetchMovies().then(setMovies).catch(console.error);
+    }, []);
+    
