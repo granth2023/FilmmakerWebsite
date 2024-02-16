0e7951f14551b6bec37a2bdf081308f0bdd07b5e
granth2023 <grantharris38@gmail.com>
2024-02-15 20:13:04 -0500

    interface added

--- frontend/my-app/movies/[id].page.tsx
+++ frontend/my-app/movies/[id].page.tsx
@@ -1,6 +1,19 @@
 import { fetchMovieById } from '../utils/movieService';
+import { GetServerSideProps } from 'next';
 
-export const getServerSideProps = async ({ params }) => {
+interface Movie {
+    id: string;
+    title: string;
+}
+
+interface MovieDetailProps {
+    movie?: Movie;
+    error?: string;
+}
+
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    if (typeof params?.id === 'string'){
     try {
         const movie = await fetchMovieById(params.id);
         return { props: { movie }};
@@ -8,8 +21,8 @@ export const getServerSideProps = async ({ params }) => {
         return { props: { error: error.message}};
     }
 };
-
-const MovieDetailPage = ({ movie, error }) => {
+};
+const MovieDetailPage = ({ movie, error }: MovieDetailProps ) => {
     if (error) {
         return <div>Error: {error}</div>
     }
