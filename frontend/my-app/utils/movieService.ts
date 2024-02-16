07ba69ca9c808b947d2902b1cb7d003eafe02263
granth2023 <grantharris38@gmail.com>
2024-02-15 20:10:45 -0500

    id added to fetch movies

--- frontend/my-app/utils/movieService.ts
+++ frontend/my-app/utils/movieService.ts
@@ -1,7 +1,7 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const fetchMovies = async () => {
-    const response = await fetch(`${API_BASE_URL}/movies`);
+export const fetchMovies = async (id: string) => {
+    const response = await fetch(`${API_BASE_URL}/movies/${id}`);
     if (!response.ok) {
         throw new Error('Failed to fetch movies');
     }
