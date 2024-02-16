f773036ecafd0edc72813e2c19583a354dc30aa5
granth2023 <grantharris38@gmail.com>
2024-02-15 20:06:51 -0500

    return props movie

--- frontend/my-app/movies/[id].page.tsx
+++ frontend/my-app/movies/[id].page.tsx
@@ -3,7 +3,7 @@ import { fetchMovieById } from '../utils/movieService';
 export const getServerSideProps = async ({ params }) => {
     try {
         const movie = await fetchMovieById(params.id);
-
+        return { props: { movie }};
     } catch (error) {
         return { props: { error: error.message}};
     }
